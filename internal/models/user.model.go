1d923720fa08e6f2f216df85d87e7ceefdaa8746
Deven-14 <deven.paramaj@gmail.com>
2023-06-15 11:59:17 +0530

    adding user model requests

--- internal/models/user.model.go
+++ internal/models/user.model.go
@@ -13,3 +13,27 @@ type User struct {
 	CreatedAt              time.Time `json:"createdAt" bson:"createdAt"`
 	UpdatedAt              time.Time `json:"updatedAt" bson:"updatedAt"`
 }
+
+type WhatsAppRequest struct {
+	WhatsAppNumber string `json:"whatsAppNumber" bson:"whatsAppNumber" binding:"required"`
+}
+
+type DiscordRequest struct {
+	DiscordId string `json:"discordId" bson:"discordId" binding:"required"`
+}
+
+type TelegramRequest struct {
+	TelegramNumber string `json:"telegramNumber" bson:"telegramNumber" binding:"required"`
+}
+
+type NotificationInterfacesRequest struct {
+	NotificationInterfaces []string `json:"notificationInterfaces" bson:"notificationInterfaces" binding:"required"`
+}
+
+type FCMtokensRequest struct {
+	FCMtokens []string `json:"FCMtokens" bson:"FCMtokens" binding:"required"`
+}
+
+type WebhooksRequest struct {
+	Webhooks []string `json:"webhooks" bson:"webhooks" binding:"required"`
+}
