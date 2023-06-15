54d7430c98ebec929d1b05fd10e044c84cba341f
Deven-14 <deven.paramaj@gmail.com>
2023-06-15 12:13:46 +0530

    added get requests in controller

--- internal/controllers/user.controller.go
+++ internal/controllers/user.controller.go
@@ -12,12 +12,25 @@ import (
 type UserController interface {
 	UpsertUser(c *gin.Context)
 	GetUser(c *gin.Context)
+
 	EditWhatsAppNumber(c *gin.Context)
+	GetWhatsAppNumber(c *gin.Context)
+
 	EditDiscordId(c *gin.Context)
+	GetDiscordId(c *gin.Context)
+
 	EditTelegramNumber(c *gin.Context)
+	GetTelegramNumber(c *gin.Context)
+
 	EditNotificationInterfaces(c *gin.Context)
+	GetNotificationInterfaces(c *gin.Context)
+
 	EditFCMtokens(c *gin.Context)
+	GetFCMtokens(c *gin.Context)
+
 	EditWebhooks(c *gin.Context)
+	GetWebhooks(c *gin.Context)
+
 	DeleteUser(c *gin.Context)
 }
 
@@ -91,6 +104,24 @@ func (controller *userController) EditWhatsAppNumber(c *gin.Context) {
 	c.JSON(http.StatusOK, whatsAppRequest)
 }
 
+func (controller *userController) GetWhatsAppNumber(c *gin.Context) {
+
+	userId, err := utils.GetUserId(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	whatsAppNumber, err := controller.userService.GetWhatsAppNumber(userId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, models.WhatsAppRequest{WhatsAppNumber: whatsAppNumber})
+
+}
+
 func (controller *userController) EditDiscordId(c *gin.Context) {
 	userId, err := utils.GetUserId(c)
 	if err != nil {
@@ -112,6 +143,24 @@ func (controller *userController) EditDiscordId(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, discordRequest)
+}
+
+func (controller *userController) GetDiscordId(c *gin.Context) {
+
+	userId, err := utils.GetUserId(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	discordId, err := controller.userService.GetDiscordId(userId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, models.DiscordRequest{DiscordId: discordId})
+
 }
 
 func (controller *userController) EditTelegramNumber(c *gin.Context) {
@@ -137,6 +186,24 @@ func (controller *userController) EditTelegramNumber(c *gin.Context) {
 	c.JSON(http.StatusOK, telegramRequest)
 }
 
+func (controller *userController) GetTelegramNumber(c *gin.Context) {
+
+	userId, err := utils.GetUserId(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	telegramNumber, err := controller.userService.GetTelegramNumber(userId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, models.TelegramRequest{TelegramNumber: telegramNumber})
+
+}
+
 func (controller *userController) EditNotificationInterfaces(c *gin.Context) {
 	userId, err := utils.GetUserId(c)
 	if err != nil {
@@ -158,6 +225,24 @@ func (controller *userController) EditNotificationInterfaces(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, notificationInterfacesRequest)
+}
+
+func (controller *userController) GetNotificationInterfaces(c *gin.Context) {
+
+	userId, err := utils.GetUserId(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	notificationInterfaces, err := controller.userService.GetNotificationInterfaces(userId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, models.NotificationInterfacesRequest{NotificationInterfaces: notificationInterfaces})
+
 }
 
 func (controller *userController) EditFCMtokens(c *gin.Context) {
@@ -183,6 +268,24 @@ func (controller *userController) EditFCMtokens(c *gin.Context) {
 	c.JSON(http.StatusOK, fcmTokensRequest)
 }
 
+func (controller *userController) GetFCMtokens(c *gin.Context) {
+
+	userId, err := utils.GetUserId(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	fcmTokens, err := controller.userService.GetFCMtokens(userId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, models.FCMtokensRequest{FCMtokens: fcmTokens})
+
+}
+
 func (controller *userController) EditWebhooks(c *gin.Context) {
 	userId, err := utils.GetUserId(c)
 	if err != nil {
@@ -204,6 +307,24 @@ func (controller *userController) EditWebhooks(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, webhooksRequest)
+}
+
+func (controller *userController) GetWebhooks(c *gin.Context) {
+
+	userId, err := utils.GetUserId(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	webhooks, err := controller.userService.GetWebhooks(userId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, models.WebhooksRequest{Webhooks: webhooks})
+
 }
 
 func (controller *userController) DeleteUser(c *gin.Context) {
